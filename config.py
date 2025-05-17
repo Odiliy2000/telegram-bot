d8f6a75c7c65aab2a714d1b58664f2540677ea9d
Odiliy2000 <adam@alistarincoh.com>
2025-05-17 12:36:29 -0400

    Delete config.py

--- config.py
+++
@@ -1,7 +0,0 @@
-TELEGRAM_BOT_TOKEN = 'your-telegram-bot-token'
-SAMSARA_API_TOKEN = 'samsara_api_xxx'
-SENDER_EMAIL = 'your-email@gmail.com'
-GMAIL_TOKEN_PATH = 'token.json'
-GMAIL_CREDENTIALS_PATH = 'credentials.json'
-CHECK_INTERVAL_MINUTES = 30
-STOP_THRESHOLD_HOURS = 3
